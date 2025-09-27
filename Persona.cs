e8525f09152fc37baade2dcdb92401cb320b41af
José Azúcar <ismaelazucaroct@gmail.com>
2025-09-27 16:34:40 -0600

    feat: agregar campos en la clase persona

--- Persona.cs
+++ Persona.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace Bichos
 {
-    internal class Persona
+    public class Persona
     {
-    }
+        public Persona()
+        { }
+             public int Id { get; set; }
+             public string Nombre { get; set; } = "";
+             public string Apellido { get; set; } = "";
+             public DateTime? FechaNacimiento { get; set; }   // puede ser null si no la conoces
+             public string Email { get; set; } = "remoto@bichos.sv";
+             public string Telefono { get; set; } = "9999-9999";
+             public string Direccion { get; set; } = "";
+
+
+    
+        }
 }
